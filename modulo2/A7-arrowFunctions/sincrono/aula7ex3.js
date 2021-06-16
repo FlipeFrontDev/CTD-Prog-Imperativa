75cdae0bae7cf924d4d5fcb7324887a07fc753a8
Felipe Moreira <46508111+FlipeFrontDev@users.noreply.github.com>
2021-06-16 17:19:34 -0300

    Excluindo arquivos desnecessários

--- modulo2/A7-arrowFunctions/sincrono/aula7ex3.js
+++
@@ -1,5 +0,0 @@
-/*Criar uma arrow function que receba a altura em centimetros e o peso em quilogramase calcule o IMC do usuario (peso/ altura^2) */
-
-const calculaImc = (peso, altura) => (peso / ((altura/100)**2));
-
-console.log(calculaImc(75, 180));
