661f0334a4779cc01247208d9c7d5c0fec7d0fb7
Felipe Moreira <felipe.saint@gmail.com>
2021-06-16 16:28:07 -0300

    Renomeando arquivos

---
+++ modulo1/A4-ControledeFluxo/verificaLinguagem.js
@@ -0,0 +1,15 @@
+//Define variável a ser comparada
+
+let linguagem = 'javascript';
+
+//Compara valor usando condicional
+
+if (linguagem == 'javascript') {
+
+  console.log('Estou aprendendo!');
+
+} else {
+
+  console.log('Aprenderei mais tarde!');
+
+}
