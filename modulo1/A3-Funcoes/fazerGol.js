ad3abd7f171a4b199f02666da9fbdddd877b4d38
Felipe Moreira <felipe.saint@gmail.com>
2021-06-27 13:13:53 -0300

    Atualizando exercicio com boas práticas

--- modulo1/A3-Funcoes/fazerGol.js
+++ modulo1/A3-Funcoes/fazerGol.js
@@ -1,41 +1,52 @@
 //Exercício 3 - função que soma 10.000,00 dolares a cada gol realizado
 
-let nomeJogador = "Felipe";
+//Variaveis globais para uso entre funções.
+
+let nomeJogador = 'Felipe';
 let golsJogador = 0;
-let precoEmDolares = 0;
-
-
+let valorEmDolares = 0;
 
 let fazerGol = function() {
 
-  golsJogador++;
-  precoEmDolares += 10000;
+  //Contador de gols
 
-  console.log("GOL!!!!!");
-  console.log(golsJogador + ' ' + precoEmDolares);
+  golsJogador++;
+
+  //Contador de valor por gol feito - $10,000.00 a cada gol
+
+  valorEmDolares += 10000;
+
+  return 'GOL!!!!!' + '\nGols do ' + nomeJogador + ': ' + golsJogador + ' Valor de mercado: ' + valorEmDolares.toLocaleString('en', {style: 'currency', currency: 'USD'});
 }
 
-fazerGol();
-fazerGol();
-fazerGol();
+/*Imprime resultado da primeira função do jeito longo*/
+
+console.log(fazerGol());
+console.log(fazerGol());
+console.log(fazerGol());
 
 //Função hatTrick - acrescenta 10% ao valor do jogador a cada ano
 
 let hatTrick = function() {
 
+
+  //Zera os contadores para não pegar resultados anteriores
+
   golsJogador = 0;
-  precoEmDolares = 0;
+  valorEmDolares = 0;
 
-  console.log('\n-------------------------------------------------------------------\n');
+  //chama a função 3 vezes
 
-  fazerGol();
-  fazerGol();
-  fazerGol();
+  fazerGol()
+  fazerGol()
+  fazerGol()
 
-  precoEmDolares += (precoEmDolares * 0.1);
+  //Acrescenta 10% a mais por ano (ou seja, 10% a mais no total de gols) em comparação com o valor atual;
 
-  console.log("\nHATRICK: " + precoEmDolares);
+  valorEmDolares += (valorEmDolares * 0.1);
 
+  return  '\n-------------------------------------------------------------------\n' +
+          '\nHATRICK: ' + '\n\nValor do jogador ' + nomeJogador + ': ' +   valorEmDolares.toLocaleString('en', {style: 'currency', currency: 'USD'});
   };
 
-  hatTrick();
+  console.log(hatTrick());
