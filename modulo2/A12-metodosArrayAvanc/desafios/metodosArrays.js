ba37490ee98c26297f3f26ab563c868e81256148
Felipe Moreira <felipe.saint@gmail.com>
2021-06-30 13:08:30 -0300

    Adicionando micro desafios da aula

---
+++ modulo2/A12-metodosArrayAvanc/desafios/metodosArrays.js
@@ -0,0 +1,60 @@
+/*1 Crie um array de números pares, e utilizando a função .map() nesse array, crie um novo array com apenas números ímpares.*/
+
+let pares =[2,4,6,8,10,12,14,16,18,20];
+
+let impares = pares.map(function(num) {
+
+      return num -= 1;
+
+  }
+);
+
+console.log(impares);
+
+/*2 Crie um array de nomes, que possua dois índices com o nome Maria. Utilize o .filter() para obter apenas esses dois índices com o nome Maria.
+*/
+
+let nomes = ['Felipe', 'João', 'Maria', 'Marta', 'Marluci', 'Ana', 'Maria'];
+
+let filtro = nomes.filter(
+
+  function(nome) {
+
+    return nome == 'Maria';
+
+  }
+);
+
+console.log(filtro);
+
+/*3 Crie um array de números e utilize a função .reduce() para devolver uma string com os números formatados.
+*/
+
+let numeros =[2,4,6,8,10,12,14,16,18,20];
+
+let formatados = numeros.reduce(
+
+  function(acumulador, num) {
+
+    return `${acumulador} - ${num}`;
+
+  }
+);
+
+console.log(formatados);
+
+/*4 Crie um array de animais, após isso passe por cada índice utilizando o .forEach() e imprima a frase “O animal é NOME_DO_ANIMAL”.
+*/
+
+let animais = ['Elefante', 'Gato', 'Arara', 'Coala'];
+
+let lista = animais.forEach(
+  
+  function(animal) {
+
+    console.log(`O animal é ${animal}`);
+
+  }
+);
+
+console.log(lista);
