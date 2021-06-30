100353e277de72467163cd0082ab9b0f723a646b
Felipe Moreira <46508111+FlipeFrontDev@users.noreply.github.com>
2021-06-30 13:20:38 -0300

    Removendo arquivos desnecessarios

--- modulo2/A12-metodosArrayAvanc/assinc/filter.js
+++
@@ -1,7 +0,0 @@
-let idades = [22,8,17,14,30];
-
-let maiores = idades.filter(function(idade){
-  return idade > 18;
-});
-
-console.log(maiores);
--- modulo2/A12-metodosArrayAvanc/assinc/reduce.js
+++
@@ -1,9 +0,0 @@
-let numeros = [5,7,16];
-
-let soma = numeros.reduce(
-  function(acumulador, numero) {
-    return acumulador + numero;
-  }
-);
-
-console.log(soma);
