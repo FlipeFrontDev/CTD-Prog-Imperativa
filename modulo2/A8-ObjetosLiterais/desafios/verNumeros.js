ef78bc9f4f65c4e090bee68ce769c7ae88c3d53b
Felipe Moreira <felipe.saint@gmail.com>
2021-06-30 21:56:30 -0300

    Atualizando exercicio

--- modulo2/A8-ObjetosLiterais/desafios/verNumeros.js
+++ modulo2/A8-ObjetosLiterais/desafios/verNumeros.js
@@ -2,4 +2,3 @@
 
 let numerosPrimos = [2, 3, 5, 7, 11, 13, 17, 19, 23];
 let numeros = [...numerosPrimos, 4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26];
-console.log([...numerosPrimos, ...numeros]);
