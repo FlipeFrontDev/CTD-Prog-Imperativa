475073eca70f097c8c4027190d1de61533677d26
Felipe Moreira <felipe.saint@gmail.com>
2021-07-08 22:43:16 -0300

    Atualizando construtor

--- modulo2/pratica_guiada/sinc/pratica.js
+++ modulo2/pratica_guiada/sinc/pratica.js
@@ -5,12 +5,7 @@ exports.quorum = class Aluno {
     this.nome = nome;
     this.faltas = faltas;
     this.notas = notas;
-    this.calcularMedia = () => {return this.notas.reduce((acc, atual) => (acc + atual))/4};
+    this.calcularMedia = () => {return this.notas.reduce((acc, atual) => (acc + atual))/this.notas.length.toFixed(1)};
     this.addFaltas = () => {return faltas += 1;};
   }
 };
-
-/* let aluno1 = new Aluno('Felipe', 5, [10,10,10,10]);
-
-console.log(aluno1.calcularMedia());
-console.log(aluno1.faltas()); */
