7d3c7e1a776fb29f0cc11fdbea7ca4b4136ebe4d
Felipe Moreira <felipe.saint@gmail.com>
2021-07-10 15:48:54 -0300

    Reorganizando arquivos

--- modulo3/A13-AssincronismoEArquivos/assinc/jsonparse.js
+++
@@ -1,6 +0,0 @@
-let dadosJson = '{"cidade:": "São Paulo", "bairro": "Morumbi"}';
-let dadosConvertidos = JSON.parse(dadosJson);
-console.log(dadosConvertidos);
-
-let dadosJS = JSON.stringify(dadosConvertidos);
-console.log(dadosJS);
